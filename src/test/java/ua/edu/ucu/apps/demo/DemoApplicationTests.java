b5aee930a0a8d74054abadf16f78ad524504b3d3
otecdima <93607175+otecdima@users.noreply.github.com>
2022-11-22 21:21:53 +0200

    systemproperties

--- src/test/java/ua/edu/ucu/apps/demo/DemoApplicationTests.java
+++ src/test/java/ua/edu/ucu/apps/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package ua.edu.ucu.apps.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package ua.edu.ucu.apps.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
